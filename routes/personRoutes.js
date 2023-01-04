09e954a2561b169a73198e559748bf55540980d3
Filipe Bacof <filipebacof@gmail.com>
2023-01-04 01:56:46 -0300

    todas as rotas para Pessoa Criadas!

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -92,4 +92,23 @@ router.patch('/:id', async (req, res) => {
     }
 })
 
+// Delete --- deletar dados
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const person = await Person.findOne({_id: id})
+    if (!person) {
+        res.status(422).json({message: 'o usuário não foi encontrado!'})
+        return
+    }
+
+    try {
+        await Person.deleteOne({_id: id})
+        res.status(200).json({message: "usuário removido com sucesso"})
+    }
+    catch (error) {
+        res.status(500).json({ error: error })
+    }
+    
+})
+
 module.exports = router
